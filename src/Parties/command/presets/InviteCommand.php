4f37946fcb43c6e45361d894da4d28f519512389
Didqh <didacatz@hotmail.com>
2019-01-16 11:50:06 +0100

    Fix grammar mistake

--- src/Parties/command/presets/InviteCommand.php
+++ src/Parties/command/presets/InviteCommand.php
@@ -22,7 +22,7 @@ class InviteCommand extends PartyCommand {
      */
     public function __construct(PartyCommandMap $map) {
         $this->plugin = $map->getPlugin();
-        parent::__construct(["invite"], "/party invite (player)", "Invites the player to your party, creating one if you haven't created it");
+        parent::__construct(["invite"], "/party invite (player)", "Invites the player to your party, creating one if you haven't done it");
     }
 
     /**
@@ -30,7 +30,7 @@ class InviteCommand extends PartyCommand {
      * @param array $args
      */
     public function onCommand(Session $session, array $args): void {
-        if(isset($args[0])) {
+        if(!isset($args[0])) {
             $session->getOwner()->sendMessage($this->getUsageMessageId());
             return;
         }
